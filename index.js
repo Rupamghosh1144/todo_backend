4a09734914b3f2a1f97802cf2e8580452bd2c0da
Rupam Ghosh <ghoshrupam1144@gmail.com>
2025-07-18 09:48:29 +0530

    First version

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import  dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.js';
+import taskRoutes from './routes/tasks.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Home Page - API is running");
+});
+
+const PORT = 5000;
+const MONGO_URI = process.env.MONGO_URI; 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+    console.log("Connected to MongoDB");
+  });
+})
+.catch((err) => {
+  console.error(" MongoDB connection error:", err);
+});
